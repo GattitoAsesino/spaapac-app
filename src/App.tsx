c2bbb02fedb27e583a35aa703d7c6bd71c46990f
GATITO-ASESINO <sheilamichelle.medina@gmail.com>
2026-08-26 23:44:46 -0600

    carga de areas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
 import { useEffect, useState } from "react";
-import Database from '@tauri-apps/plugin-sql';
+import {Area,getAreas} from './db.ts'; 
+import './App.css';
+
 
 function App() {
-  const [areas, setAreas] = useState<any[]>([]);
+  const [areas, setAreas] = useState<Area[]>([]);
+  const [loading, setLoading] = useState <boolean>(true);
+  const [error, setError] = useState <string | null>(null);
 
   useEffect(() => {
     async function cargarDatos() {
-      const db = await Database.load('sqlite:areas.db');
-      const result = await db.select('SELECT * FROM areas');
-      console.log(result);
-      setAreas(result as any[]);
+      try {
+        const result = await getAreas();
+        setAreas(result);
+        
+      } catch (e:unknown) {
+        if(e instanceof Error){ //type guard, vereficamos que lo que nos boto si es un error
+          setError(e.message);
+        }
+        else{
+          setError("Error desconocido");
+        }
+      }finally {
+        setLoading(false);//pases lo q pase debe acabar de cargar
+      }
+      
     }
     cargarDatos();
   }, []);
 
   return (
-    <div>
-      <p>Áreas en DB: {areas.length}</p>
-    </div>
-  );
+  <div>
+    {loading && <p>Cargando áreas...</p>}
+    
+    {error && <p>Error: {error}</p>}
+    
+    {!loading && !error && (
+      <div>
+        <p>Áreas en DB: {areas.length}</p>
+        <ul>
+          {areas.map((area) => (
+            <li key={area.id}>{area.nombre}</li>
+          ))}
+        </ul>
+      </div>
+    )}
+  </div>
+);
 }
 
 export default App;
