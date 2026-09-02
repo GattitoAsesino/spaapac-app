6c53dfc0195ee4e1a016a55671e28c8c38c99825
GATITO-ASESINO <sheilamichelle.medina@gmail.com>
2026-09-02 00:08:32 -0600

    calcular xp

--- src/xp.ts
+++ src/xp.ts
@@ -4,12 +4,14 @@ export function calcularNivelYEtapa(xpTotal: number) {
 
   let nivel = 1;
   let xpAcumulado = 0;
+  let etapa = 1;
 
   while (xpAcumulado < xpTotal) {
     const xpParaSiguienteNivel = base * Math.pow(nivel, exponente);
     xpAcumulado += xpParaSiguienteNivel;
     nivel++;
   }
+  etapa = Math.floor(nivel / 2);
 
-  return { nivel };
+  return { nivel, etapa };
 }
